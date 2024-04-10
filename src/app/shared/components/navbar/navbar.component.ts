59dd774221569ca43df82ef34e39e50945d2164c
Hilario Arevalo <hilarioarevalo03@gmail.com>
2024-04-09 18:24:48 -0600

    Btterment of navpage and frames of subtitles

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,31 @@
-import {Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-navbar',
   templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  public navegacionResponsive(): void {
+
+    const navegacion = document.querySelector('.navegacion')
+    if (navegacion?.classList.contains('mostrar')) {
+      navegacion.classList.remove('mostrar');
+    } else {
+      navegacion?.classList.add('mostrar');
+    }
+  }
+
+  public showMenu(): void {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu?.addEventListener('click', this.navegacionResponsive)
+  }
+
+  public darkMode(): void {
+    const btnDarkMode = document.querySelector('.dark-mode-boton');
+    btnDarkMode?.addEventListener('click', function () {
+      document.body.classList.toggle('bg-neutral-400');
+    });
+  }
+}
